9d98fcad8edae613faf62551887dc59f527c4055
Hudson <hudson.farias.dev@gmail.com>
2024-02-12 14:10:13 -0300

    API - ignore repos

--- backend/routers/infos.py
+++ backend/routers/infos.py
@@ -8,6 +8,7 @@ router = APIRouter()
 @router.get("/infos", status_code = 200, response_model = InfosModel)
 async def contact():
     repos = get('https://api.github.com/users/hudson-farias/repos').json()
+    ignore_repos = [752588874, 750776309]
 
     response = InfosModel(
         roles = ['Desenvolvedor de Software', 'Fullstack', 'Backend', 'Frontend', 'Devops'],
@@ -17,7 +18,8 @@ async def contact():
             whatssap = 'https://wa.me/message/GIRAZSPEDZSXE1',
             discord = 'https://discord.com/users/1127594477536694332'
         ),
-        repos = [ReposModel(**repo) for repo in repos]
+        repos = [ReposModel(**repo) for repo in repos if repo['id'] not in ignore_repos]
     )
+
 
     return response
